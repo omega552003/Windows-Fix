ad73de11a31ae5b9c27dfcfa3f5785fa3f8b1e00
omega552003 <omega552003@users.noreply.github.com>
2022-12-15 21:05:21 -0500

    Update fixWin.bat

--- fixWin.bat
+++ fixWin.bat
@@ -1,4 +1,7 @@
 sfc /scannow
-dism /Online /Cleanup-Image /CheckHealth
-dism /Online /Cleanup-Image /ScanHealth
-dism /Online /Cleanup-Image /RestoreHealth
+Dism /Online /Cleanup-Image /CheckHealth
+Dism /Online /Cleanup-Image /ScanHealth
+Dism /Online /Cleanup-Image /RestoreHealth
+REM Dism /Online /Cleanup-Image /RestoreHealth /Source:wim:H:\Sources\Install.wim:1
+REM Dism /Online /Cleanup-Image /RestoreHealth /Source:esd:H:\Sources\Install.esd:1
+sfc /scannow
