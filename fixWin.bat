b4a864bb81ee8119c9d01f2933164d589d7b2108
omega552003 <omega552003@users.noreply.github.com>
2023-05-18 17:01:19 -0400

    Update fixWin.bat

--- fixWin.bat
+++ fixWin.bat
@@ -1,7 +1,11 @@
-sfc /scannow
+sfc /verifyonly
 DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /ScanHealth
 DISM /Online /Cleanup-Image /RestoreHealth
+REM Use release source
 REM DISM /Online /Cleanup-Image /RestoreHealth /Source:H:\Sources\Install.wim
 REM DISM /Online /Cleanup-Image /RestoreHealth /Source:H:\Sources\Install.esd
+REM
+REM Limit only to install file
+REM DISM /Online /Cleanup-Image /RestoreHealth /Source:H:\Sources\Install.esd /LimitAccess
 sfc /scannow
